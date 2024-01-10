f4f1637b169bef3ca39d0e8a32554fecab9c30ca
IgLemp <iglemp@protonmail.com>
2024-01-10 15:07:57 +0100

    changes

--- scripts/czytanie_danych_lista_produktow.py
+++ scripts/czytanie_danych_lista_produktow.py
@@ -1,29 +1,29 @@
 import csv
 import os
 
-# Inicjalizacja listy produktów
-products_data = []
-
 # Nazwa pliku CSV
 csv_file = os.path.join('ListaProduktow.csv')
 
-# Wczytywanie danych z pliku CSV
-with open(csv_file, 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        # Zapisywanie danych do listy
-        product_info = {
-            'ID Produktu': row['ID Produktu'],
-            'Nazwa Produktu': row['Nazwa Produktu'],
-            'Waga (kg)': float(row['Waga (kg)']),  # Konwersja wagi na float dla poprawnego sortowania
-            'Wymiary (mm)': row['Wymiary (mm)'],
-        }
-        products_data.append(product_info)
+def load_data() -> list:
+    products_data = []
+    # Wczytywanie danych z pliku CSV
+    with open(csv_file, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # Zapisywanie danych do listy
+            product_info = {
+                'ID Produktu': row['ID Produktu'],
+                'Nazwa Produktu': row['Nazwa Produktu'],
+                'Waga (kg)': float(row['Waga (kg)']),  # Konwersja wagi na float dla poprawnego sortowania
+                'Wymiary (mm)': row['Wymiary (mm)'],
+            }
+            products_data.append(product_info)
 
-# Sortowanie listy produktów według wagi (malejąco)
-products_data = sorted(products_data, key=lambda x: x['Waga (kg)'], reverse=True)
+    # Sortowanie listy produktów według wagi (malejąco)
+    products_data = sorted(products_data, key=lambda x: x['Waga (kg)'], reverse=True)
 
-# Wyświetlanie wczytanych i posegregowanych danych
-for product_info in products_data:
-    print(f"ID: {product_info['ID Produktu']}, Dane: {product_info}")
+    # Wyświetlanie wczytanych i posegregowanych danych
+    for product_info in products_data:
+        print(f"ID: {product_info['ID Produktu']}, Dane: {product_info}")
 
+    return products_data
---
+++ scripts/sortowanie_produktów.py
@@ -0,0 +1,5 @@
+from czytanie_danych_lista_produktow import load_data
+
+data = load_data()
+
+
