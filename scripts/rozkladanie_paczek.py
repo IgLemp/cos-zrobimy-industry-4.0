4014420bcda64fd9ca9ee1aaf4c71acacc9c5d39
Filipo009 <jakfilipp@gmail.com>
2024-01-10 21:13:14 +0100

    Więcej paczek jest wciskanych na paletę. Paczki nie koniecznie są tego samego rozmiaru

--- scripts/rozkladanie_paczek.py
+++ scripts/rozkladanie_paczek.py
@@ -30,36 +30,34 @@ palette_dimensions = {'Szerokość': 800, 'Długość': 1200, 'Wysokość': 1856
 current_palette = {'Szerokość': 0, 'Długość': 0, 'Wysokość': 0}
 number_of_packages = 0
 palette_number = 1
-current_layer_height = 0
+remaining_height = palette_dimensions['Wysokość']  # Dodana zmienna do śledzenia brakującej wysokości
+stack_number = 1  # Numer stosu paczek na palecie
 
 # Funkcja do dodawania paczki na palecie
 def add_package_to_palette(product_info):
-    global current_layer_height  # Dodano deklarację jako zmiennej globalnej
+    global remaining_height, stack_number
     dimensions_str = product_info['Wymiary (mm)']
     dimensions = [int(dim) for dim in dimensions_str.split('x')]
     package_dimensions = dimensions
 
-    # Sprawdzenie czy paczka zmieści się na aktualnej palecie i warstwie
-    if (current_palette['Szerokość'] + package_dimensions[0] <= palette_dimensions['Szerokość'] and
-            current_palette['Długość'] + package_dimensions[1] <= palette_dimensions['Długość'] and
-            current_layer_height + package_dimensions[2] <= palette_dimensions['Wysokość']):
-        current_palette['Szerokość'] += package_dimensions[0]
-        current_palette['Długość'] += package_dimensions[1]
-        current_layer_height += package_dimensions[2]
-        print(f"Dodano paczkę ID {product_info['ID Produktu']} na palecie {palette_number}. Wymiary paczki: {dimensions_str}. Wysokość warstwy: {current_layer_height}.")
-        return True
-    elif (current_palette['Szerokość'] + package_dimensions[1] <= palette_dimensions['Szerokość'] and
-          current_palette['Długość'] + package_dimensions[0] <= palette_dimensions['Długość'] and
-          current_layer_height + package_dimensions[2] <= palette_dimensions['Wysokość']):
-        # Spróbuj umieścić paczkę w innej orientacji
-        current_palette['Szerokość'] += package_dimensions[1]
-        current_palette['Długość'] += package_dimensions[0]
-        current_layer_height += package_dimensions[2]
-        print(f"Dodano paczkę ID {product_info['ID Produktu']} na palecie {palette_number}. Wymiary paczki (inna orientacja): {dimensions_str}. Wysokość warstwy: {current_layer_height}.")
+    # Sprawdzenie czy paczka zmieści się na aktualnej palecie
+    if current_palette['Wysokość'] + package_dimensions[2] <= palette_dimensions['Wysokość']:
+        current_palette['Wysokość'] += package_dimensions[2]
+        remaining_height = palette_dimensions['Wysokość'] - current_palette['Wysokość']
+        print(f"Paczka ID {product_info['ID Produktu']} na palecie {palette_number}, stos {stack_number}. Wymiary paczki: {dimensions_str}. Wolna przestrzeń: {remaining_height} mm.")
         return True
     else:
-        # Paczka nie zmieściła się na aktualnej palecie, rozpocznij nową
-        return False
+        # Paczka nie zmieści się na aktualnej palecie, sprawdź czy można umieścić jedna na drugiej
+        if package_dimensions[2] <= remaining_height:
+            remaining_height -= package_dimensions[2]
+            stack_number += 1
+            current_palette['Wysokość'] = package_dimensions[2]
+            print(f"Paczka ID {product_info['ID Produktu']} na palecie {palette_number}, stos {stack_number}. Wymiary paczki: {dimensions_str}. Wolna przestrzeń (po umieszczeniu jedna na drugiej): {remaining_height} mm.")
+            return True
+        else:
+            # Paczka nie zmieściła się ani na palecie, ani jedna na drugiej, rozpocznij nową paletę
+            print(f"Paczce ID {product_info['ID Produktu']} brakuje miejsca na palecie. Wolna przestrzeń: {remaining_height} mm.")
+            return False
 
 # Iteracja przez posegregowane paczki i dodawanie paczek na palety
 for product_info in products_data:
@@ -68,14 +66,12 @@ for product_info in products_data:
     # Spróbuj dodać paczkę do aktualnej palety
     if not add_package_to_palette(product_info):
         # Jeżeli paczka nie zmieści się na aktualnej palecie, rozpocznij nową paletę
-        print(f"\nZaczynam nową paletę {palette_number}. Ilość paczek na poprzedniej palecie: {number_of_packages}.")
+        print(f"\nZaczynam nową paletę {palette_number}. Ilość paczek na poprzedniej palecie: {number_of_packages}")
         current_palette = {'Szerokość': 0, 'Długość': 0, 'Wysokość': 0}
-        current_layer_height = 0
+        remaining_height = palette_dimensions['Wysokość']
         number_of_packages = 1
         palette_number += 1
-        # Spróbuj dodać paczkę do nowej palety
-        if not add_package_to_palette(product_info):
-            print(f"Nie udało się umieścić paczki ID {product_info['ID Produktu']} na nowej palecie.")
+        stack_number = 1  # Reset numeru stosu
 
 # Wyświetlanie informacji o ostatniej palecie
-print(f"\nOstatnia paleta {palette_number}. Ilość paczek na palecie: {number_of_packages}. Wysokość warstwy: {current_layer_height}.")
+print(f"\nOstatnia paleta {palette_number}. Ilość paczek na palecie: {number_of_packages}. Wolna przestrzeń: {remaining_height} mm.")
