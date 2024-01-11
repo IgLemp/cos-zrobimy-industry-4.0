39691fc9754bc6b85466d47e0bcea0610a6ff4ad
IgLemp <iglemp@protonmail.com>
2024-01-11 08:40:28 +0100

    semi working...

---
+++ app/drawing.py
@@ -0,0 +1,100 @@
+from PIL import Image, ImageDraw
+
+def load_warehouse_map() -> dict:
+    with open('./data/warehouse.txt', 'r') as file:
+    # Inicjalizujemy pusty słownik
+        magazyn_dict = {}
+
+        # Iterujemy po każdej linii w pliku
+        for line in file:
+            # Dzielimy linię na kolumny (współrzędne X, Y, klucz)
+            columns = line.strip().split('\t')
+
+            # Tworzymy wpis w słowniku
+            x, y, key = int(columns[0]), int(columns[1]), columns[2]
+            magazyn_dict[key] = {'X': x, 'Y': y}
+    return magazyn_dict
+
+magazyn_dict = load_warehouse_map()
+
+def przyporzadkuj(liczba):
+    if liczba == 1 or liczba == 2 or liczba==15 or liczba==16:
+        return 1
+    elif 3==liczba or liczba== 4 or liczba==14 or liczba==13:
+        return 2
+    elif 5 == liczba or liczba == 6 or liczba==12 or liczba ==11:
+        return 3
+    elif 7 == liczba or liczba== 8 or liczba==10 or liczba==9:
+        return 4
+
+def rysuj_kwadrat(lista_punktow):
+    map_image = Image.open("/data/maps/mapa.jpg")
+    draw = ImageDraw.Draw(map_image)
+    for x, y in lista_punktow:
+        draw.rectangle([x - 12.5, y-12.5, x + 12.5, y+12.5], fill=(255, 0, 0))
+    map_image.save("/data/maps/pomoc.jpg")
+
+def rysuj_prosta(lista_punktow, filename):
+    map_image = Image.open("/data/maps/pomoc.jpg")
+    draw = ImageDraw.Draw(map_image)
+    for i in range(len(lista_punktow) - 1):
+        (x1, y1) = lista_punktow[i]
+        (x2, y2) = lista_punktow[i + 1]
+        draw.line([(x1, y1), (x2, y2)], fill=(255, 0, 0), width=5)
+
+    map_image.save(f"/data/maps/{filename}")
+# Otwieramy plik magazyn.txt do odczytu
+
+
+
+def generate_path_image(filename):
+    kroki = []
+    with open('/data/maps/zapis_róchów.txt', 'r') as file:
+        for line in file:
+            kroki.append(line.strip())
+    kwadraty=[]
+    linie=[]
+    pocz=0
+    koniec=len(kroki)
+    if kroki[0][5::] not in ["20","00","40"]:
+        pocz+=1
+        liczba=przyporzadkuj(int(kroki[0][5::]))
+        if int(kroki[1][2:4])%2==0:
+            kwadraty.append((magazyn_dict[kroki[1]]['X']+95*(8-liczba), magazyn_dict[kroki[1]]['Y']))
+            if int(kroki[1][5::])%2==0:
+                linie.append((magazyn_dict[kroki[1]]['X']+95*(8-liczba), magazyn_dict[kroki[1]]['Y']-50))
+            else:
+                linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']+95*(8-liczba), magazyn_dict[kroki[1]]['Y']+50))
+            linie.append((magazyn_dict[kroki[1]]['X']+95*(8-liczba), magazyn_dict[kroki[1]]['Y']))
+        else:
+            kwadraty.append((magazyn_dict[kroki[1]]['X']-95*(liczba), magazyn_dict[kroki[1]]['Y']))
+            if int(kroki[0][5::])%2==0:
+                linie.append((magazyn_dict[kroki[1]]['X']-95*(liczba), magazyn_dict[kroki[1]]['Y']+50))
+            else:
+                linie.append((magazyn_dict[kroki[1]]['X']-95*(liczba), magazyn_dict[kroki[1]]['Y']-50))
+            linie.append((magazyn_dict[kroki[1]]['X']-95*(liczba), magazyn_dict[kroki[1]]['Y']))
+    if kroki[len(kroki)-1][5::] not in ["20","00","40"]:
+        koniec-=1
+    for i in range(pocz,koniec):
+        kwadraty.append((magazyn_dict[kroki[i]]['X'], magazyn_dict[kroki[i]]['Y']))
+        linie.append((magazyn_dict[kroki[i]]['X'], magazyn_dict[kroki[i]]['Y']))
+
+    if kroki[len(kroki)-1][5::] not in ["20","00","40"]:
+        liczba=przyporzadkuj(int(kroki[len(kroki)-1][5::]))
+        if int(kroki[len(kroki)-1][2:4])%2==0:
+            kwadraty.append((magazyn_dict[kroki[len(kroki)-2]]['X']-95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']))
+            linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']-95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']))
+            if int(kroki[len(kroki)-1][5::])%2==0:
+                linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']-95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']-50))
+            else:
+                linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']-95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']+50))
+        else:
+            kwadraty.append((magazyn_dict[kroki[len(kroki)-2]]['X']+95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']))
+            linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']+95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']))
+            if int(kroki[len(kroki)-1][5::])%2==0:
+                linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']+95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']+50))
+            else:
+                linie.append((magazyn_dict[kroki[len(kroki)-2]]['X']+95*(5-liczba), magazyn_dict[kroki[len(kroki)-2]]['Y']-50))
+
+    rysuj_kwadrat(kwadraty)
+    rysuj_prosta(linie, filename)
---
+++ app/movement.py
@@ -0,0 +1,155 @@
+# polorzenie_pocznotkowe1="H106A00"
+# polorzenie_koncowe1="H404A06"
+
+def gdzie_idziesz(polorzenie_koncowe, polorzenie_pocznotkowe):
+    polorzenie_pocznotkowe = list(polorzenie_pocznotkowe)
+    polorzenie_koncowe = list(polorzenie_koncowe)
+    prubna_list=[polorzenie_koncowe,polorzenie_pocznotkowe]
+    if(polorzenie_pocznotkowe[5] !=0 and polorzenie_pocznotkowe[6] !=0):
+        prubna_list=wychodzenie_z_alejek_bocznych(polorzenie_koncowe, polorzenie_pocznotkowe)
+    if(prubna_list[1][1]==prubna_list[0][1]):
+        print( "".join(prubna_list[1]),6)
+        zmienne_dane.append("".join(prubna_list[1]))
+        polorzenie_koncowe=prubna_list[0]
+        polorzenie_pocznotkowe=prubna_list[1]
+
+        if(polorzenie_pocznotkowe[3]!=polorzenie_koncowe[3]):
+            if(int(polorzenie_koncowe[5])*10+int(polorzenie_koncowe[6])>8):
+                polorzenie_pocznotkowe[3]=polorzenie_koncowe[3]
+                print( "".join(polorzenie_pocznotkowe),12)
+                zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                polorzenie_pocznotkowe[6]=polorzenie_koncowe[6]
+                polorzenie_pocznotkowe[5]=polorzenie_koncowe[5]
+            elif(polorzenie_pocznotkowe[3]>=polorzenie_koncowe[3]):
+                if(int(polorzenie_koncowe[3])==1):
+                    polorzenie_pocznotkowe[3]=str(int(polorzenie_koncowe[3])+1)
+                else:
+                    polorzenie_pocznotkowe[3]=str(int(polorzenie_koncowe[3])-1)
+                print( "".join(polorzenie_pocznotkowe),18)
+                zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                if(int(polorzenie_koncowe[3])%2==0):
+                    polorzenie_pocznotkowe[5]=str(4)
+                    polorzenie_pocznotkowe[6]=str(0)
+                    print( "".join(polorzenie_pocznotkowe),19)
+                    zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                else:
+                    polorzenie_pocznotkowe[5]=str(2)
+                    polorzenie_pocznotkowe[6]=str(0)
+                    print( "".join(polorzenie_pocznotkowe),13)
+                    zmienne_dane.append("".join(polorzenie_pocznotkowe))
+            elif(polorzenie_pocznotkowe[3]<=polorzenie_koncowe[3]):
+                if(int(polorzenie_koncowe[3])==7):
+                    polorzenie_pocznotkowe[3]=str(int(polorzenie_koncowe[3])-1)
+                else:
+                    polorzenie_pocznotkowe[3]=str(int(polorzenie_koncowe[3])+1)
+                print( "".join(polorzenie_pocznotkowe),22)
+                zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                if(int(polorzenie_koncowe[3])%2==0):
+                    polorzenie_pocznotkowe[5]=str(4)
+                    polorzenie_pocznotkowe[6]=str(0)
+                    print( "".join(polorzenie_pocznotkowe),19)
+                    zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                else:
+                    polorzenie_pocznotkowe[5]=str(2)
+                    polorzenie_pocznotkowe[6]=str(0)
+                    print( "".join(polorzenie_pocznotkowe),13)
+                    zmienne_dane.append("".join(polorzenie_pocznotkowe))
+            polorzenie_pocznotkowe[3]=str(int(polorzenie_koncowe[3]))
+            print( "".join(polorzenie_pocznotkowe),14)
+            zmienne_dane.append("".join(polorzenie_pocznotkowe))
+            polorzenie_pocznotkowe[6]=polorzenie_koncowe[6]
+            polorzenie_pocznotkowe[5]=polorzenie_koncowe[5]
+        prubna_list=[polorzenie_koncowe,polorzenie_pocznotkowe]
+        zmienne_dane.append("".join(prubna_list[1]))
+        print( "".join(prubna_list[1]),11)
+        prubna_list[0]="".join(prubna_list[0])
+        prubna_list[1]="".join(prubna_list[1])
+        return(prubna_list)
+        
+        
+    else:
+        zmienne_dane.append("".join(prubna_list[1]))
+        print("".join(prubna_list[1]),7)
+        prubna_list=poruszanie_po_halah(polorzenie_koncowe, polorzenie_pocznotkowe)
+        
+        return(prubna_list)
+        
+
+def wychodzenie_z_alejek_bocznych(polorzenie_koncowe, polorzenie_pocznotkowe):
+    if((int(polorzenie_pocznotkowe[5])*10)+int(polorzenie_pocznotkowe[6])<20):
+        if((int(polorzenie_pocznotkowe[5])*10)+int(polorzenie_pocznotkowe[6])<=8):
+            polorzenie_pocznotkowe[6]=str(0)
+        elif(int(polorzenie_pocznotkowe[3])%2==0):
+            polorzenie_pocznotkowe[6]=str(0)
+            polorzenie_pocznotkowe[5]=str(2)
+        else:
+            polorzenie_pocznotkowe[6]=str(0)
+            polorzenie_pocznotkowe[5]=str(4)
+        zmienne_dane.append("".join(polorzenie_pocznotkowe))
+        print("".join(polorzenie_pocznotkowe),1)
+        if(int(polorzenie_pocznotkowe[5])>=2 and int(polorzenie_pocznotkowe[5])<4 ):
+            if(int(polorzenie_pocznotkowe[3])%2==0):
+                polorzenie_pocznotkowe[3]=str(int(polorzenie_pocznotkowe[3])-1)
+                zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                print("".join(polorzenie_pocznotkowe),2)
+            
+            polorzenie_pocznotkowe[5]=str(0)
+            polorzenie_pocznotkowe[6]=str(0)
+            zmienne_dane.append("".join(polorzenie_pocznotkowe))
+            print("".join(polorzenie_pocznotkowe),3)
+        elif(int(polorzenie_pocznotkowe[5])>=4):
+            if(int(polorzenie_pocznotkowe[3])%2!=0):
+                polorzenie_pocznotkowe[3]=str(int(polorzenie_pocznotkowe[3])+1)
+                zmienne_dane.append("".join(polorzenie_pocznotkowe))
+                print("".join(polorzenie_pocznotkowe),4)
+            polorzenie_pocznotkowe[5]=str(0)
+            polorzenie_pocznotkowe[6]=str(0)
+            zmienne_dane.append("".join(polorzenie_pocznotkowe))
+            print("".join(polorzenie_pocznotkowe),5)
+    prubna_list=[polorzenie_koncowe,polorzenie_pocznotkowe]
+    return(prubna_list)
+
+def poruszanie_po_halah(polorzenie_koncowe, polorzenie_pocznotkowe):
+    hala_docelowa = int(polorzenie_koncowe[1])
+    hala_początkowy = int(polorzenie_pocznotkowe[1])
+    if(hala_początkowy == 1):
+            polorzenie_pocznotkowe[1] = str(hala_początkowy + 2)
+            polorzenie_pocznotkowe[3] = str(1)
+    elif(hala_docelowa % 2 == hala_początkowy % 2):
+        if (hala_docelowa>hala_początkowy): #porószanie dla magazynów góra dół 
+            polorzenie_pocznotkowe[1] = str(hala_początkowy + 2)
+            polorzenie_pocznotkowe[3] = str(1)
+        elif (hala_docelowa < hala_początkowy):
+            polorzenie_pocznotkowe[1] = str(hala_początkowy - 2)
+            polorzenie_pocznotkowe[3] = str(7)
+    elif(hala_docelowa % 2 != 0):#porószanie dla magaznów prawo lewo z początkowym nie parzystym
+        polorzenie_pocznotkowe[3] = str(5)
+        zmienne_dane.append("".join(polorzenie_pocznotkowe))
+        print( "".join(polorzenie_pocznotkowe),8)
+        polorzenie_pocznotkowe[1] = str(hala_początkowy + 1)
+    elif(hala_początkowy % 2 != 0):#porószanie dla magaznów prawo lewo z docelowym parzystym
+        polorzenie_pocznotkowe[3] = str(2)
+        zmienne_dane.append("".join(polorzenie_pocznotkowe))
+        print( "".join(polorzenie_pocznotkowe),9)
+        polorzenie_pocznotkowe[1] = str(hala_początkowy - 1)
+
+    return(gdzie_idziesz(polorzenie_koncowe, polorzenie_pocznotkowe))
+
+
+zmienne_dane=[]
+
+def generate_path(polorzenie_pocznotkowe1, polorzenie_koncowe1):
+    # polorzenie_pocznotkowe1="H106A00"
+    # polorzenie_koncowe1="H202A01"
+
+    zmienne_dane.append(polorzenie_pocznotkowe1)
+    prubna_list=[polorzenie_pocznotkowe1,polorzenie_koncowe1]
+    prubna_list= gdzie_idziesz(polorzenie_koncowe1, polorzenie_pocznotkowe1)
+
+    with open('/data/map/zapis_róchów.txt','w') as plik:
+        for i in zmienne_dane:
+            plik.write(str(i+"\n"))
+    
+    zmienne_dane.clear()
+
+# print(polorzenie_pocznotkowe1)
